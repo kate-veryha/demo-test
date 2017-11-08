b3592e8c271b044da1b60ff906a04fe2193a981e
Kate Veryha <katrusia.veryha@gmail.com>
2017-11-08 13:53:20 +0200

    added material ui

---
+++ src/userForm.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Paper from 'material-ui/Paper';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+const styles = {
+  radioButtonGroup: {
+    marginTop: 20,
+  },
+  radioButton: {
+    marginBottom: 16,
+  },
+  form: {
+    marginRight: '5%',
+  },
+  flatButton: {
+    marginTop: 20,
+  }
+};
+
+
+export default class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validation = {
+      firstName: { rule: /^[A-Za-z\-\s]+$/, errText: 'Can only contain letters, hyphens and spaces', touched: false, valid: false},
+      lastName: { rule: /^[A-Za-z\-\s]+$/, errText: 'Can only contain letters, hyphens and spaces', touched: false, valid: false},
+      gender: { rule: /male|female/, errText: '', touched: false, valid: false},
+      age: { rule: /^[1-9][0-9]$/, errText: 'Must be in range from 1 to 99', touched: false, valid: false},
+      phone: { rule: /^\d{3,12}$/, errText: 'Can only contain 3 to 12 digits', touched: false, valid: false},
+    };
+  }
+
+  handleSubmit(e) {
+    if (this.isFormValid()) {
+      for (let key in this.validation) {
+        if (this.validation.hasOwnProperty(key)) {
+          this.validation[key].touched = false;
+          this.validation[key].valid = false;
+        }
+      }
+      this.props.addUser();
+      e.target.reset();
+    } else {
+    }
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    const target = e.target;
+    this.validation[target.name].touched = true;
+      this.validation[target.name].valid = UserForm
+        .isFieldValid(target.value, this.validation[target.name].rule);
+    this.props.changeData(target.name, target.value);
+  }
+
+  static isFieldValid(str, regexp) {
+    return regexp.test(str);
+  }
+
+  isFormValid() {
+    let isValid = true;
+    for (let key in this.validation) {
+      if (this.validation.hasOwnProperty(key)) {
+        isValid = isValid && this.validation[key].valid;
+      }
+    }
+    return isValid;
+  }
+
+  msg(key) {
+    return  this.validation[key].touched && !this.validation[key].valid
+      ? this.validation[key].errText
+      : '';
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <Paper zDepth={2}>
+          <form onSubmit={this.handleSubmit}>
+            <TextField name='firstName'
+                       onChange={ this.handleChange }
+                       errorText={ this.msg('firstName') }
+                       floatingLabelText='First name'/>
+            <TextField name='lastName'
+                       onChange={ this.handleChange }
+                       errorText={ this.msg('lastName')}
+                       floatingLabelText='Last name'/>
+            <TextField name='phone'
+                       onChange={ this.handleChange }
+                       errorText={ this.msg('phone')}
+                       floatingLabelText='Phone'/>
+            <RadioButtonGroup
+                              name='gender'
+                              onChange={ this.handleChange }
+                              style={styles.radioButtonGroup}>
+              <RadioButton
+                label='Male'
+                style={styles.radioButton}
+                value='male'/>
+              <RadioButton
+                label='Female'
+                style={styles.radioButton}
+                value='female'/>
+            </RadioButtonGroup>
+            <TextField name='age'
+                       floatingLabelText='Age'
+                       type='number'
+                       onChange={ this.handleChange }
+                       errorText={ this.msg('age') }/>
+            <FlatButton label='Add user'
+                        style={styles.flatButton}
+                        type='submit'
+                        primary={true}/>
+          </form>
+        </Paper>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+UserForm.PropTypes = {
+  user: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    phone: PropTypes.string,
+    gender: PropTypes.string,
+    age: PropTypes.string,
+  }),
+  addUser: PropTypes.func,
+  changeData: PropTypes.func,
+};
---
+++ src/userTable.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Paper from 'material-ui/Paper';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
+import './index.css';
+import trashImg from './delete-button.svg';
+
+const styles = {
+  paper: {
+    width: '60%',
+    marginLeft: '5%',
+  },
+  table: {
+    height: '400px',
+  },
+  tableHeader: {
+    cursor: 'pointer',
+  },
+  svg: {
+    width: 20,
+  }
+};
+
+const ClickableTableRowColumn = (props) => {
+  // Wrapper to add onMouseDown handler
+  // Needed because original lib component has issues
+  const {rowData, ...restProps} = props;
+  return (
+    <TableRowColumn
+      {...restProps}
+      onMouseDown={props.onMouseDown}>
+    </TableRowColumn>
+  )
+};
+
+const ClickableTableHeaderColumn = (props) => {
+  const {order, ...restProps} = props;
+  const arrows = {
+    '1': '\u2191',
+    '-1': '\u2193',
+    'null': ''
+  };
+  return (
+    <TableHeaderColumn
+      {...restProps}
+      onMouseDown={props.onMouseDown}
+    >
+      {props.children}
+      <span>{arrows[order]}</span>
+    </TableHeaderColumn>)
+};
+
+export default class UserTable extends Component {
+  constructor(props) {
+    super(props);
+    this.sortDirections = [1, -1, null];
+  }
+
+  generateCols() {
+    const cols = this.props.cols;
+    const sort = this.props.sortPresets || false;
+    return cols.map((colData) =>
+      <ClickableTableHeaderColumn key={colData.key}
+                                  order={(sort.col === colData.key) ? this.props.sortPresets.dir : null}
+                                  style={styles.tableHeader}
+                                  tooltip='Click to sort'
+                                  onMouseDown={() => { this.changeSortOrder(colData.key) }}>
+        {colData.label}
+      </ClickableTableHeaderColumn>);
+  }
+
+  handleClickDelete(item) {
+    this.props.deleteUser(item);
+  }
+
+  sortCompareFunction(a, b) {
+    const aSort = a[this.props.sortPresets.col].toString().toLowerCase();
+    const bSort = b[this.props.sortPresets.col].toString().toLowerCase();
+    if (aSort < bSort) {
+      return -1 * this.props.sortPresets.dir;
+    }
+    if (aSort > bSort) {
+      return 1 * this.props.sortPresets.dir;
+    }
+    return 0;
+  }
+
+  next(array, currentElem) {
+    const nextElemIndex = array.findIndex((elem) => elem === currentElem) + 1;
+    return array[nextElemIndex !== array.length ? nextElemIndex : 0];
+  }
+
+  changeSortOrder(col) {
+    debugger;
+    const set = {
+      col: col,
+      dir: (col === this.props.sortPresets.col)
+        ? this.next(this.sortDirections, this.props.sortPresets.dir)
+        : this.sortDirections[0]
+    };
+    this.props.setSortOrder(set);
+  }
+
+  generateRows(users) {
+    const cols = this.props.cols;
+    return users.map( item => {
+      const cells = cols.map((col) => <TableRowColumn>{item[col.key]}</TableRowColumn> );
+      return (<TableRow key={item.surname}
+                        className='table-row'>{cells}
+                        <ClickableTableRowColumn className='delete-button'
+                                                 onMouseDown={ this.handleClickDelete.bind(this, item) }
+                                                 rowData={item}>
+                          <img src={trashImg} alt='Delete button' style={styles.svg}/>
+                        </ClickableTableRowColumn>
+      </TableRow>)
+    });
+  }
+
+  render() {
+    const renderedUsers = (this.props.sortPresets.dir === null)
+      ? this.props.users
+      : this.props.users.slice().sort(this.sortCompareFunction.bind(this));
+    const headers = this.generateCols();
+    const rows = this.generateRows(renderedUsers);
+    return (
+      <MuiThemeProvider>
+        <Paper zDepth={2} style={styles.paper}>
+          <Table height={styles.table.height}>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>{headers}<TableHeaderColumn>To trash</TableHeaderColumn></TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              {rows}
+            </TableBody>
+          </Table>
+        </Paper>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+UserTable.PropTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    phone: PropTypes.string,
+    gender: PropTypes.string,
+    age: PropTypes.string,
+  })),
+  deleteUser: PropTypes.func,
+  sortPresets: PropTypes.any,
+  setSortOrder: PropTypes.func,
+  cols: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string,
+    label: PropTypes.string,
+  }))
+};
